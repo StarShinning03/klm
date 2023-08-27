89dba701e07afb1b4ccf039341742d0d0e2ebb78
StarShinning03 <132030548+StarShinning03@users.noreply.github.com>
2023-08-27 20:59:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Shinningstar03/OfficialMusic",
+    "https://github.com/TheVepeX",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
