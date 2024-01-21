3c9b9afdf73f07ba71841e29f64579f8b2531c6e
StarShinning03 <132030548+StarShinning03@users.noreply.github.com>
2024-01-21 10:29:22 +0530

    Update sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -44,7 +44,7 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-@app.on_message(filters.command(["sudolist", "listsudo", "sudoers"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["Frm45s", "Frm46s", "Frm48s"]) & ~BANNED_USERS)
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
